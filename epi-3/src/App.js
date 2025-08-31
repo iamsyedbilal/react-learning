3462caa64bc99e7408ed57b55a6ae2cce8db0ea3
iamsyedbilal <syedbilal.dev27@gmail.com>
2025-08-31 18:05:59 +0500

    Understanding componnets / diff between react & JSX , Class & Fun Components etc.

---
+++ epi-3/src/App.js
@@ -0,0 +1,15 @@
+//* React Components
+// Class Base (Older way of creating the components)
+// Function Base (New way to create the components, Now 99% dev use function components)
+
+// React functional components is just a JS function which return some piece of JSX
+
+function App() {
+  return (
+    <div>
+      <h1> from the App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ epi-3/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// This one is called component composition where we render component inside component
+root.render(<App />);
